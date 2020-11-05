34e98026b4367f4b6335cb67e226ce385d34be33
Sonali Najera <sonalitzul@gmail.com>
2020-11-04 20:39:13 -0800

    Updates

---
+++ precache-manifest.a5ac63397469d1bb10d4291aca1f0883.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b393a2de1c84ca3040587f6ca395af8c",
+    "url": "/sonali-portfolio/index.html"
+  },
+  {
+    "revision": "85a84b6a950fc16e325c",
+    "url": "/sonali-portfolio/static/css/main.f1fd84d4.chunk.css"
+  },
+  {
+    "revision": "02fdcac4f12688e96b19",
+    "url": "/sonali-portfolio/static/js/2.ccf3f2a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sonali-portfolio/static/js/2.ccf3f2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a84b6a950fc16e325c",
+    "url": "/sonali-portfolio/static/js/main.09a70dd9.chunk.js"
+  },
+  {
+    "revision": "43b57383fa550c799904",
+    "url": "/sonali-portfolio/static/js/runtime-main.5229589b.js"
+  },
+  {
+    "revision": "71399d43a4bad546ba392422e84b55c6",
+    "url": "/sonali-portfolio/static/media/burntToast.71399d43.png"
+  },
+  {
+    "revision": "0adab17e8a8e0200e9ce009db8f6fabd",
+    "url": "/sonali-portfolio/static/media/labBook.0adab17e.png"
+  },
+  {
+    "revision": "1f9d00790e2d20c7b5e9091b81c53aa2",
+    "url": "/sonali-portfolio/static/media/selfie.1f9d0079.png"
+  },
+  {
+    "revision": "2d2bbdd4d482248a20a347e612f3b367",
+    "url": "/sonali-portfolio/static/media/spacedRep.2d2bbdd4.png"
+  }
+]);
